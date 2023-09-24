84bb5a164b13c781000988547d4316bc998b842b
Siva70136 <94431863+Siva70136@users.noreply.github.com>
2023-09-24 19:17:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://blackbox-lemon.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
