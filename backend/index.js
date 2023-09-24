3592c490dde15038b6c267e24564af31b879dbed
Siva70136 <94431863+Siva70136@users.noreply.github.com>
2023-09-24 19:29:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
-//const RegisterModel = require('./models/Register')
+
 
 const app = express()
 
