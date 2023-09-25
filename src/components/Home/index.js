f23af999d33042de59c150be5c4a94d4f7f613bf
Siva70136 <94431863+Siva70136@users.noreply.github.com>
2023-09-25 19:32:24 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -144,6 +144,17 @@ const Home = (props) => {
                                     <th><h3 className="texthead">Description</h3></th>
                                 </tr>
                             </thead>
+                                <tbody>
+                                {data.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.name}</td>
+                                        <td>{item.category}</td>
+                                        <td>{item.price}</td>
+                                        <td>{item.quantity}</td>
+                                        <td>{item.description}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
                             
                         </table>
                     </div>
